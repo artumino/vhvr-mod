0f149dbd01ddf3e06c67dd144ead626486495b4a
Andi1986 <andreas.hof@gmx.de>
2021-05-30 03:32:54 +0200

    wip

--- ValheimVRMod/Scripts/VRPlayerSync.cs
+++ ValheimVRMod/Scripts/VRPlayerSync.cs
@@ -11,37 +11,54 @@ namespace ValheimVRMod.Scripts {
         public GameObject leftHand = new GameObject();
 
         private void Awake() {
-            GetComponent<ZNetView>().Register("VrSyncObjects", new Action<long, Transform, Transform, Transform>(RPC_VrSyncObjects));
+            if (GetComponent<Player>() == Player.m_localPlayer) {
+                GetComponent<ZNetView>().Register("VrSyncObjects", new Action<long, ZPackage>(RPC_VrSyncObjects));
+            }
         }
 
-        private void RPC_VrSyncObjects(long sender, Transform cam, Transform lHand, Transform rHand) {
+        private void RPC_VrSyncObjects(long sender, ZPackage pkg) {
             
+            foreach (Player player in Player.GetAllPlayers()) {
+                if (player.GetComponent<ZNetView>().GetZDO().m_owner == sender) {
+                    player.GetComponent<VRPlayerSync>().UpdateOtherPlayer(pkg);
+                }
+            }
+        }
+
+        public void UpdateOtherPlayer(ZPackage pkg) {
+
             Debug.Log("Invoked RPC_VrSyncObjects !");
-            camera.transform.position = cam.position;
-            camera.transform.rotation = cam.rotation;
-            leftHand.transform.position = lHand.position;
-            leftHand.transform.rotation = lHand.rotation;
-            rightHand.transform.position = rHand.position;
-            rightHand.transform.rotation = rHand.rotation;
-            
+            camera.transform.position = pkg.ReadVector3();
+            camera.transform.rotation = pkg.ReadQuaternion();
+            leftHand.transform.position = pkg.ReadVector3();;
+            leftHand.transform.rotation = pkg.ReadQuaternion();;
+            rightHand.transform.position = pkg.ReadVector3();;
+            rightHand.transform.rotation = pkg.ReadQuaternion();;
+
             if (vrikInitialized) {
                 return;
             }
 
             VrikCreator.initialize(GetComponent<Player>().gameObject, leftHand.transform,
                 rightHand.transform, camera.transform);
-            
+
             vrikInitialized = true;
-
+            
         }
-
+        
         private void Update() {
 
             Player player = GetComponent<Player>();
 
             if (player == Player.m_localPlayer) {
-                GetComponent<ZNetView>().InvokeRPC(ZNetView.Everybody, "VrSyncObjects", 
-                    camera.transform, leftHand.transform, rightHand.transform);
+                ZPackage pkg = new ZPackage();
+                pkg.Write(camera.transform.position);
+                pkg.Write(camera.transform.rotation);
+                pkg.Write(leftHand.transform.position);
+                pkg.Write(leftHand.transform.rotation);
+                pkg.Write(rightHand.transform.position);
+                pkg.Write(rightHand.transform.rotation);
+                GetComponent<ZNetView>().InvokeRPC(ZNetView.Everybody, "VrSyncObjects", pkg);
             }
         }
     }
